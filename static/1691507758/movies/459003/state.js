df90c0606a99b3416f6dc64eaae9b640a6d585dd
redhatdevn16 <redhatdevn16@gmail.com>
2023-08-08 23:16:23 +0800

    Updates

---
+++ static/1691507758/movies/459003/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fmovie-app\u002Fstatic\u002F1691507758",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F459003",config:{_app:{basePath:b,assetsPath:b,cdnURL:a}}}}(null,"\u002Fmovie-app\u002F"));
